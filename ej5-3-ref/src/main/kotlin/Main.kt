948ae483ca226f1e2a18ba18ada180a1a52ed9f9
MaykaGR <92048821+MaykaGR@users.noreply.github.com>
2021-12-22 21:22:18 +0100

    Update Main.kt

--- ej5-3-ref/src/main/kotlin/Main.kt
+++ ej5-3-ref/src/main/kotlin/Main.kt
@@ -31,6 +31,48 @@ fun tlf(palabra: String): Boolean{
     if(palabra.first().isDigit()){return true}
     else return false
 }
+//Función que se realiza a cabo con el comando especial filtrar
+fun filtrar(agenda: Agenda){
+        var name = ""
+        var phone = ""
+    //Tras introducir filtrar, la función pide el texto a filtrar
+        println("Introduce nombre o tlf a buscar")
+    //Se pasa lo que se introduzca a mayúsculas para que la búsqueda sea independiente de cómo se introduzca el nombre
+        var comparador = readLine()?.toUpperCase()?: ""
+    //Se busca si el contacto está en la agenda
+        if (agenda.buscar(comparador) == true) {
+            // Si lo está, miramos si lo introducido es el nombre o el número para mostrar el resto de datos
+            if (comparador in agenda.datos.keys) {
+                println(agenda.datos[comparador])
+            } else {
+                println(agenda.datos.filterValues { it.contains(comparador) })
+            }
+        }
+        // Sino lo está, introducimos un nuevo contacto
+        else {
+            println("El contacto no está en agenda, vamos a incluirlo")
+            if (tlf(comparador) == true) {
+                println("Introduce nombre: ")
+                name = readLine()?.toUpperCase() ?: ""
+                phone = comparador
+            } else if (nombre(comparador) == true) {
+                println("Introduce tlf: ")
+                phone = readLine() ?: ""
+                name = comparador
+            } else {
+                println("error")
+            }
+            agenda.add(Contacto.crearContacto(name, phone))
+        }
+    }
+//Función para el comando especial listado, que imprime el listado de la agenda
+//La acción de mostrar los contactos ordenados por orden alfabético también la incluyo en esta función
+//Así la muestro directamente ordenada
+fun imprimir(agenda: Agenda){
+    //for(i in 0..agenda.datos.size){}
+    var agendaOrdenada = agenda.datos.toSortedMap()
+    println(agendaOrdenada)
+}
 
 fun main() {
     // Instancia de clase agenda
@@ -39,47 +81,21 @@ fun main() {
     val contacto = Contacto("LAURA", "857438287")
     // Añadir contacto a agenda
     agenda.add(contacto)
-    println("Introduce nombre o tlf a buscar: ")
-    //Se pasa lo que se introduzca a mayúsculas para que la búsqueda sea independiente de cómo se introduzca el nombre
-    var comparador = readLine()?.toUpperCase()?: ""
-    // Comprobamos si el contacto está en la agenda
-    var name = ""
-    var phone = ""
-    if (agenda.buscar(comparador)==true){
-        // Si lo está, miramos si lo introducido es el nombre o el número para mostrar el resto de datos
-        if(comparador in agenda.datos.keys){
-            println(agenda.datos[comparador])
-        } else {println(agenda.datos.filterValues{ it.contains(comparador)})}
+    var accion: String = ""
+    //Bucle para que se lleve a cabo usándolo según los comandos especiales
+    do {
+        println("Introduce accion (filtrar, listado o adios): ")
+        //Se pasa lo que se introduzca a mayúsculas para que la búsqueda sea independiente de cómo se introduzca el nombre
+        accion = readLine()?.toUpperCase() ?: ""
+        when(accion){
+            "FILTRAR" -> filtrar(agenda)
+            "LISTADO" -> imprimir(agenda)
+            "ADIOS" -> System.exit(0)
+            else -> println("Comando desconocido")
+        }
+
     }
-    // Sino lo está, introducimos un nuevo contacto
-    else{
-        println("El contacto no está en agenda, vamos a incluirlo")
-            if(tlf(comparador)==true){
-            println("Introduce nombre: ")
-             name = readLine()?.toUpperCase()?: ""
-             phone =comparador}
-            else if(nombre(comparador)==true){
-            println("Introduce tlf: ")
-            phone = readLine()?: ""
-            name = comparador}
-            else {println("error")}
-        agenda.add(Contacto.crearContacto(name,phone))
-    }
-    println(agenda)
-    // Comandos especiales
-    println("Introduce lo que quieres hacer ahora (adios, listado, filtra): ")
-    when(readLine()?: ""){
-        "adios" -> System.exit(0)
-        "listado" -> println(agenda)
-        "filtra" -> println("¿Qué deseas buscar?")
-        else -> println("Comando no reconocido")
-    }
-    comparador = readLine()?.toUpperCase()?: ""
-    if(comparador in agenda.datos.keys){
-        println(agenda.datos.filterKeys{ it.contains(comparador)})
-    }
-    else if(comparador in agenda.datos.values){
-        println(agenda.datos.filterValues{ it.contains(comparador)})
-    }
-    else println("Ningún contacto")
+        //Se repite hasta que se teclee adios
+        while(accion !== "ADIOS")
+        System.exit(0)
 }
